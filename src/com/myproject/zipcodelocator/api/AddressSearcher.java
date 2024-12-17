4b04dc8f0518083d2c3d7c8fb5ad513708c7a651
João Ribas <joaovitormribas@gmail.com>
2024-12-17 20:33:31 -0300

    initializing address search project by zip code

---
+++ src/com/myproject/zipcodelocator/api/AddressSearcher.java
@@ -0,0 +1,11 @@
+package com.myproject.zipcodelocator.api;
+
+import com.myproject.zipcodelocator.models.Address;
+
+public class AddressSearcher {
+    private String cep;
+
+    public Address getAddress(String cep){
+        return null;
+    }
+}
---
+++ src/com/myproject/zipcodelocator/models/Address.java
@@ -0,0 +1,10 @@
+package com.myproject.zipcodelocator.models;
+
+public class Address {
+    private String cep;
+    private String street;
+    private String neighborhood;
+    private String city;
+    private String state;
+    private int ddd;
+}
